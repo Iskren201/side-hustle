76d3ab58708a06ea5081539d6753f73ac647f882
Iskren Iliev <96354322+Iskren201@users.noreply.github.com>
2025-09-08 15:57:56 +0300

    Vercel

--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://localhost:4200',  // реалният домейн на API-то
+    apiBaseUrl: 'https://side-hustle-sepia.vercel.app/',  // реалният домейн на API-то
     withCredentials: true
 };
