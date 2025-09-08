739a3e85804a5c796a95028d285db05f672c779f
Iskren Iliev <96354322+Iskren201@users.noreply.github.com>
2025-09-08 17:32:12 +0300

    commit

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/AppModule';
-import { environment } from './environments/environments';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
